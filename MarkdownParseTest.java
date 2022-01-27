51f10558fe3f0544eab268874062085a955e7298
suprithk <suprith.krishnakumar@gmail.com>
2022-01-26 18:52:34 -0800

    Updated Junit tests
    Added more test cases

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,4 +20,22 @@ public class MarkdownParseTest {
 
         assertEquals(List.of("https://something.com", "some-page.html"), links);
     }
+
+    @Test
+    public void fileWithImage() throws IOException {
+        Path fileName = Path.of("image.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        assertEquals(List.of("google.com"), links);
+    }
+
+    @Test
+    public void incorrectFormat() throws IOException {
+        Path fileName = Path.of("incorrect.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        assertEquals(List.of(), links);
+    }
 }
